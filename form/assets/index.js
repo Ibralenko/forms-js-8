84eae849a80a7d559e05da33040ea275d7d74790
Julie <ibralenko6724809@gmail.com>
2023-07-01 20:40:45 +0300

    added working functions with a password

--- form/assets/index.js
+++ form/assets/index.js
@@ -10,37 +10,42 @@ const inputConfirmPassword = registrationForm.elements.passwordRepeat;
 const inputCheckbox = registrationForm.elements.checkbox;
 
 
+function ValidateEmail(email) {
 
-    function ValidateEmail() {
-        const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-        if (userEmail.value.match(mailFormat)) {
-            return true;
-        } else {
-            registerForm.querySelector(".error-email").innerHTML = 'Введен неверный адрес'
-            return false;
-        }
-    }
+};
 
-function checkPassword() {
-    if (inputPassword.value.patternMismatch & inputConfirmPassword.value.patternMismatch ) {
+function checkPassword(password) {
+    if (password.value.patternMismatch & password.value.patternMismatch) {
         return true;
     } else {
         document.querySelector('.error-password').innerHTML = 'Введите пароль согласно правилам';
         return false;
     }
+};
+
+function checkPasswords(password1, password2){
+    if (password1 === password2) {
+        return true;
+    } else {
+        document.querySelector('.error-password2').innerHTML = 'Пароли не совпадают'
+    }
 }
 
+function checkAgreement(checkbox){}
 if (inputCheckbox.checked) {
 	button.disabled = false;
 	} else {
 	button.disabled = true;
-    }
+};
 
 
 registrationForm.addEventListener('submit', function(event){
     event.preventDefault();
+    ValidateEmail(inputUserEmail.value)
+    checkPassword(inputPassword);
+    checkPasswords(inputPassword.value, inputConfirmPassword.value);
+    checkAgreement(inputCheckbox);
     console.log('click');
-
 });
 
 
