9c14b3863b6896f1183024a1f855691172601fc7
Julie <ibralenko6724809@gmail.com>
2023-07-01 23:24:01 +0300

    add func email

--- form/assets/index.js
+++ form/assets/index.js
@@ -18,35 +18,36 @@ function checkName(name){
     }
 };
 
-/*function ValidateEmail(email) {
-    if (email.value === " "){
-        document.querySelector('.error-email').innerHTML = 'Введите email'
-    } else if (email.value.patternMismatch){
+
+function validateEmail(email) {
+    const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailFormat)) {
         return true;
+    }else if (email.value === ''){
+        document.querySelector('.error-email').innerHTML = 'Введите email'
     } else {
         document.querySelector('.error-email').innerHTML = 'Введите корректный email'
-    }
-};*/
+        return false;
+    }};
+
 
 function checkPassword(password) {
-    if (password.value.patternMismatch && password.value.patternMismatch)  {
-        return true;
-    }else if(password.value === ''){
+    if (password === '')  {
         document.querySelector('.error-password').innerHTML = 'Введите пароль';
+    } else if(password.patternMismatch && password.patternMismatch ){
+        return true;
     } else {
-        document.querySelector('.error-password').innerHTML = 'Введите корректный пароль';
-        return false;
+    document.querySelector('.error-password').innerHTML = 'Введите корректный пароль';
     }
 };
 
 function checkPasswords(password1, password2){
-    if (password1 === password2) {
+    if (password1.value === password2.value) {
         return true;
     } else {
         document.querySelector('.error-password2').innerHTML = 'Пароли не совпадают'
     }
 };
-
 
 function checkAgreement(checkbox){
 if (checkbox.checked) {
@@ -60,11 +61,14 @@ registrationForm.addEventListener('submit', function(event){
     event.preventDefault();
     checkName (inputUserName);
     checkAgreement(inputCheckbox);
-    // ValidateEmail(inputUserEmail);
-    checkPassword(inputPassword);
-    checkPasswords(inputPassword.value, inputConfirmPassword.value);
+    validateEmail(inputUserEmail);
+    checkPassword(inputPassword.value);
+    checkPasswords(inputPassword, inputConfirmPassword);
     console.log('click');
 
+    if (checkAgreement, checkName === true){
+    console.log('Отлично! Вы зарегистрированы')
+    }
 });
 
 
