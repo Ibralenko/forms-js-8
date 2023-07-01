fb0cd6f43d1532f41b2341f138e4c19af3842fb9
Julie <ibralenko6724809@gmail.com>
2023-07-01 17:33:29 +0300

    add check radio

--- form/assets/index.js
+++ form/assets/index.js
@@ -1,14 +1,50 @@
-const form = document.forms.regictrationForm;
-const button = document.querySelector('.btn');
-const checkValidatation = ;
+const registrationForm = document.forms.regictrationForm;
+const button = form.elements.submit;
+const userName = form.elements.name;
+const userEmail = form.elements.email;
+const userAge =form.elements.age;
+const userSex =form.elements.gender;
+const profession = form.elements.profession;
+const password = form.elements.password;
+const confirmPassword = form.elements.passwordRepeat;
+const checkbox = form.elements.checkbox;
 
-function sendForm(){
 
+    function ValidateEmail() {
+        const errorEmail = registerForm.querySelector(".error-message");
+        const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if (userEmail.value.match(mailFormat)) {
+            return true;
+        } else {
+            console.log("Ваш адрес электронной почты введён неверно!");
+            return false;
+        }
+    }
+
+
+// function checkPassword (){
+//     if (password.value.match){
+//         return true;
+//     } else {
+//         password.textContent = 'Введите пароль согласно правилам';
+//         return false;
+//     }
+// }
+
+let isChecked = false;
+if (checkbox.checked) {
+    isChecked = true;
+} else {
+    button.disabled = true;
 }
+
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
     console.log('click')
+    ValidateEmail()
+
+    form.reset()
 });
 
 
