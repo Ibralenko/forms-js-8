737b330782dd4883ab6ad92f8a53d7a4760c340d
Julie <ibralenko6724809@gmail.com>
2023-07-01 15:42:17 +0300

    start js

---
+++ form/assets/index.js
@@ -0,0 +1,14 @@
+const form = document.forms.regictrationForm;
+const button = document.querySelector('.btn');
+const checkValidatation = ;
+
+function sendForm(){
+
+}
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log('click')
+});
+
+
